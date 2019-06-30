6d4912a41193562929cb819a05edd1dc112a8132
Oleksandr Stoliar <ostoliar@MacBook-Pro-Oleksandr.local>
2019-06-30 18:45:58 +0300

    header done

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('.header_search_bar_img_active').click(function() {
+        $('.header_search_bar').toggleClass('header_search_bar_active')
+    })
+    $('.header_search_bar_img_active').click(function() {
+        $('.header_search_bar_input').toggleClass('header_search_bar_input_higher');
+    })
+})
