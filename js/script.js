3ac5f739e7aa6dc82150098fcd18ec876b9648e2
Oleksandr Stoliar <ostoliar@MacBook-Pro-Oleksandr.local>
2019-07-04 12:41:34 +0300

    correct some design

--- js/script.js
+++ js/script.js
@@ -4,5 +4,13 @@ $(document).ready(function() {
     })
     $('.header_search_bar_img_active').click(function() {
         $('.header_search_bar_input').toggleClass('header_search_bar_input_higher');
-    })
+    });
 })
+
+
+// $(function () {
+//     $(".header_search_bar_img_active").unbind('click');
+//     $(".header_search_bar_img_active").click(function () {
+//         $(this).toggleClass("fas fa-search fas fa-search-minus");
+// })
+// });
